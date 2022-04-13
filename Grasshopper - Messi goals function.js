45888b88bc96c49dc7c1f54f6143bce718789c8f
WinnieSu0918 <su830918ggg@yahoo.com.tw>
2022-04-13 23:10:42 +0800

    feat: 2022-04-12 Add Solutions
    Description:
      - Dollars and Cents
      - Grasshopper - If/else syntax debug
      - Grasshopper - Messi goals function
      - Squash the bugs

---
+++ Grasshopper - Messi goals function.js
@@ -0,0 +1,25 @@
+/*
+  Title:
+    Grasshopper - Messi goals function
+    
+  Description:
+    Messi goals function
+    Messi is a soccer player with goals in three leagues:
+
+        LaLiga
+        Copa del Rey
+        Champions
+        Complete the function to return his total number of goals in all three leagues.
+
+  Examples:
+    5, 10, 2  -->  17
+    
+  Notes:
+    the input will always be valid.
+    
+*/
+
+// Now Solution
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+}
